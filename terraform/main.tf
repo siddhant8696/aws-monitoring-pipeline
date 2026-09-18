1a087e145abc29e87cd770b4cd9abb6e9e132b7f
Siddhant Raut <siddhantraut89@gmail.com>
2026-09-17 20:26:22 -0400

    Add nginx playbook and HTTP access to security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,6 +63,14 @@ resource "aws_security_group" "ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
